6fb2c0a30f61fa9dbd1709c3eb2d33a4cb1a327f
Albert Cheruiyot <bettalbert99@gmail.com>
2024-03-25 15:40:32 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let employee;
+employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    employee[key] = value;
+    return obj;
+}
+
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = { ...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
